169a720eaff18782c01933f5745986641f8a73bd
m-gouda <goda61@gmail.com>
2023-11-07 13:20:34 +0200

    Add Media and static settings and image to models

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Post
+
+
+
+class ProductAdmin (admin.ModelAdmin) :
+    list_display = ['title','draft']
+    list_filter = ['draft','tags']
+    search_fields = ['title','tags']
+
+
+
+admin.site.register(Post,ProductAdmin)
