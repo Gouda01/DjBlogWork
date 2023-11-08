4b29de290df209168917756d1f855d099bdde105
m-gouda <goda61@gmail.com>
2023-11-08 11:54:02 +0200

    Add Category and Comments to models

--- posts/admin.py
+++ posts/admin.py
@@ -3,15 +3,17 @@ from django.contrib import admin
 # Register your models here.
 
 
-from .models import Post
+from .models import Post , Category , Comment
 
 
 
 class ProductAdmin (admin.ModelAdmin) :
-    list_display = ['title','draft']
+    list_display = ['title','category','draft']
     list_filter = ['draft','tags']
     search_fields = ['title','tags']
 
 
 
 admin.site.register(Post,ProductAdmin)
+admin.site.register(Category)
+admin.site.register(Comment)
--- posts/models.py
+++ posts/models.py
@@ -1,19 +1,41 @@
 from django.db import models
 from django.utils import timezone
 from taggit.managers import TaggableManager
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 
 class Post (models.Model) :
-    title = models.CharField(max_length=100)
-    content = models.TextField(max_length=20000)
-    draft = models.BooleanField(default=True)
-    publish_date = models.DateTimeField(default=timezone.now)
-    tags = TaggableManager()
-    image = models.ImageField(upload_to='post')
+    
+    author          = models.ForeignKey(User,related_name='post_author',on_delete=models.CASCADE)
 
+    title           = models.CharField(max_length=100)
+    content         = models.TextField(max_length=20000)
+    draft           = models.BooleanField(default=True)
+    publish_date    = models.DateTimeField(default=timezone.now)
+    tags            = TaggableManager()
+    image           = models.ImageField(upload_to='post')
+
+    category        = models.ForeignKey('Category',related_name='post_category',on_delete=models.SET_NULL,null=True)
+
+    
+    def __str__(self):
+        return self.title
+    
+class Category(models.Model) :
+    title = models.CharField(max_length=100)
 
     def __str__(self):
         return self.title
+    
+class Comment (models.Model) :
+    post        = models.ForeignKey(Post,related_name='post_comment',on_delete=models.CASCADE)
+    user        = models.CharField(max_length=50)
+    comment     = models.TextField(max_length=300)
+    created_at  = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self) :
+        return str(self.post)
