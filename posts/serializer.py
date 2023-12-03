19c08adca756ad077b67792cc3fcc558e535ba20
Mohamed Gouda <goda61@gmail.com>
2023-12-03 12:42:43 +0200

    Show relationship as nasted objects in API

--- posts/serializer.py
+++ posts/serializer.py
@@ -1,13 +1,19 @@
 from rest_framework import routers, serializers, viewsets
 from .models import Post
+from django.contrib.auth.models import User
 
 
 
 # Serializers define the API representation.
 
+class UserSerializer(serializers.ModelSerializer) :
+    class Meta :
+        model = User
+        fields = ['id','username','email']
+
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField()
+    author = UserSerializer()
     category = serializers.StringRelatedField()
 
     class Meta:
