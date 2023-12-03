eee205cc533f93c5230160c68547c1d7f8c34123
Mohamed Gouda <goda61@gmail.com>
2023-12-03 12:07:44 +0200

    Add swagger docs

--- project/urls.py
+++ project/urls.py
@@ -22,6 +22,25 @@ from django.conf.urls.static import static
 from posts.views import post_list , post_details  ,create_post ,edit_post , delete_post , PostList , PostDetail , EditPost , CreatePost, DeletePost
 from posts.api import post_list_api , post_detail_api , PostListApi, PostDetailApi
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+...
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog API",
+      default_version='v1',
+      description="Blog description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -43,6 +62,10 @@ urlpatterns = [
 
     path('posts/api',PostListApi.as_view()),
     path('posts/<int:pk>/api',PostDetailApi.as_view()),
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+   path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+   path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
 
